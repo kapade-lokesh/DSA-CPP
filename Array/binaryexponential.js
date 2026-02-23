e828aac8399ab15984b7a8e266b99941abbbc30f
kapade-lokesh <lkkapade@gmail.com>
2026-02-23 11:31:26 +0530

    changes in binaryexpo

--- Array/binaryexponential.js
+++ Array/binaryexponential.js
@@ -1,15 +1,23 @@
-const calculatePower = (base,power)=>{
+// Binary Power Calculator Function 
+
+const powerFunc = (base,power) => {
     let ans=1;
+
+    if(power<0){
+      base = 1/base;
+      power = -power;
+    }
+
     while(power>0){
       
-        if(power%2==1){
-         ans = ans *number;
-        }
-        number = number*number;
-         power = Math.floor(power / 2);
+      if(power%2==1){
+          ans=ans*base;
+      }
+      base = base*base;
+      power=Math.floor(power/2);
+      
     }
-    return ans
+  return ans
 }
-
-let result = calculatePower(3,10);
+const result = powerFunc(3,1)
 console.log(result);
